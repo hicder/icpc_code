2e9e9182c3988d1759c2dcd1cf51f6cabdaf250b
hicder <hieup@outlook.com>
2013-04-29 14:37:13 -0500

    hmm

---
+++ graph.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<map>
+#include<queue>
+#include<stack>
+#include<vector>
+
+#DEFINE DFS_WHITE 0
+#DEFINE DFS_BLACK 1
+
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+vector<vii> AdjList;
+int nvertices, nedges;
+
+vi bfs_num;
+
+
+void initialize(){
+    AdjList.resize(nvertices);
+    dfs_num.resize(nvertices);
+    for (int i = 0; i < nvertices; i++) {
+        dfs_num[i] = DFS_WHITE;
+    }
+}
+
+void bfs(int start){
+    vi s;
+    s[start] = 0;
+    queue qnodes;
+    qnodes.push(start);
+    while (!qnodes.empty()) {
+        int u = qnodes.front();qnodes.pop();
+        bfs_num[u] = DFS_BLACK;
+        for (int i = 0; i < AdjList[u].size(); i++) {
+            ii v = AdjList[u][i];
+            if (bfs_num[v.first]) {
+                s[v.first] = s[u] + 1;  //distance from the start
+                qnodes.push(v.first);
+            }
+        }
+        
+    }
+}
+
+/*****************
+Let's say here's the input:
+nvertices nedges
+##next nedges lines are those edges, which are undirected
+******************/
+
+void input(){
+    cin >> nvertices >> nedges;
+    initialize();
+    int u, v;
+    for (int i = 0;  i< nedges; i++) {
+        cin >> u >> v;
+        vi k(v, 1);
+        AdjList[u].push(k);
+        vi k2(u, 1);
+        AdjList[v].push(k2);
+    }
+}
+
+
+int main(){
+    input();
+    for (int i = 0; i < nvertices; i++) {
+        if (!bfs_num[i]) {
+            bfs(i);
+        }
+    }
+}
