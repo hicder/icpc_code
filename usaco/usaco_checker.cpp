1d553f298c51e55c3cd3206d5935248586264f7e
hicder <hieup@outlook.com>
2013-05-20 13:02:42 +0700

    usaco

--- usaco/usaco_checker.cpp
+++ usaco/usaco_checker.cpp
@@ -1,41 +1,111 @@
+/*
+ID: phamduc1
+PROG: checker
+LANG: C++
+*/
+
 #include <iostream>
 #include <cstdio>
 #include <string.h>
 #include <vector>
 using namespace std;
-int board[13][13];
-int n;
-int diagrow[13];
-int nprint;
-int trans(int t){
-	if(t == 0) return t;
-	if(t < n) return t;
-	int x = t;
-	while(x >= n){
-		if(x < n) break;
-		if(x %n == 0){
-			x--;
-			continue;
+int board[14][14];
+int n=0;
+int backdiag[26];
+int forediag[26];
+int rowplaced[14];
+int nprint=0;
+int nsol=0;
+int back_trans(int row, int col){
+	if (col >= row){
+		return (col - row);
+	}
+	else{
+		return ((row - col) + (n-1));
+	}
+}
+
+int fore_trans(int row, int col){
+	return (row + col);
+}
+
+bool canplace(int row, int col){
+	if((rowplaced[row] == 0) && (backdiag[back_trans(row,col)] == 0) && (forediag[fore_trans(row,col)] == 0)){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void print(){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(board[j][i]){
+				cout << (j+1);
+
+				if(i != (n - 1)) {
+					cout << " ";
+
+				}
+				break;
+			}
 		}
-		
-		x -= (n + 1);
+	}
+	cout << endl;
+}
+
+void printcerr(){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(board[j][i]){
+				cerr << (j+1);
+				if(i != (n - 1)) {
+					cerr << " ";
+				}
+				break;
+			}
+		}
+	}
+	cerr << endl;
+}
+
+void place(int col){
+	if(col == n) {
+		nsol ++;
+		if(nprint < 3){
+			print();
+			nprint++;
+		}
+		return;
 	}
 	
-	return x;
+	for(int i = 0; i < n; i++){
+		if(canplace(i, col)){
+			rowplaced[i] = 1;
+			board[i][col] = 1;
+			backdiag[back_trans(i,col)] = 1;
+			forediag[fore_trans(i,col)] = 1;
+			place(col + 1);
+			backdiag[back_trans(i,col)] = 0;
+			forediag[fore_trans(i,col)] = 0;
+			board[i][col] = 0;
+			rowplaced[i] = 0;
+		}
+	}
 }
-
-int cdiag(int t){
-	if(diagrow[trans(t)]) return 0;
-	else return 1;
-}
-
-
 
 int main(){
+	freopen("checker.in","r",stdin);
+	freopen("checker.out","w",stdout);
 	memset(board,0,sizeof(board));
-	memset(diagrow,0,sizeof(diagrow));
-	nprint = 0;
+	memset(backdiag,0,sizeof(backdiag));
+	memset(forediag,0,sizeof(forediag));
+	memset(rowplaced,0,sizeof(rowplaced));
 	cin >> n;
-	
+	nsol = 0;
+	nprint = 0;
+	place(0);
+	cout<<nsol<<endl;
 	return 0;
 }
