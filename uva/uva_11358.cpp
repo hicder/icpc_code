3f8d6ddeadf92ba06482506c09e72108e5042376
Duc Hieu Pham <hieup@outlook.com>
2013-07-08 20:05:48 +0700

    11358

---
+++ uva/uva_11358.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <cstring>
+#define INF 128
+#define MAXV 50102
+#define monk(i) (2+(i))
+#define Inte(i) (2 + (n) + (i))
+#define pb push_back
+#define FOR(i, n) for(int i = 0; i < (n); i++)
+#define pf printf
+using namespace std;
+
+int main(){
+	return 0;
+}
