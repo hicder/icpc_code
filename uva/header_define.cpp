9f5346793ed8a869be7162e194ab35d10ee8bba5
Duc Hieu Pham <hieup@outlook.com>
2013-08-24 23:20:03 -0500

    header fix

--- uva/header_define.cpp
+++ uva/header_define.cpp
@@ -8,19 +8,23 @@
 #include <utility>
 #include <cstring>
 #include <sstream>
+#include <cstring>
+#include <string>
 #define INF 1000000000
 #define MAXV 50102
 #define pb push_back
 #define FOR(i, n) for(int i = 0; i < (n); i++)
 #define REP(i,a,b) for(int i = (a); i < (b); i++)
 #define pf printf
-
+#define MS(a) memset((a), 0, sizeof((a)))
 using namespace std;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef vector<vi> vvi;
 typedef vector<ii> vii;
-int main(){
 
+
+int main(){
+freopen("input", "r", stdin);
 	return 0;
 }
